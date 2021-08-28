eb702f33b4db52665a0fc4c6fb44d355702875b2
allenw238 <allenwu750@gmail.com>
2021-08-28 14:46:36 -0700

    CSES Problem Set, Introductory Problems: Permutations
    AC

---
+++ cses/introductory-problems/Permutations.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n; cin >> n;
+  if (n == 1) {
+    cout << 1 << endl;
+    return 0;
+  }
+  if (n < 4) {
+    cout << "NO SOLUTION" << endl;
+    return 0;
+  }
+  for (int i = 2; i <= n; i += 2) cout << i << " ";
+  for (int i = 1; i <= n; i += 2) cout << i << " ";
+  cout << endl;
+  return 0;
+}
