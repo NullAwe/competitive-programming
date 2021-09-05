065d798da86daf6b86b90b3eef6288d29b863708
allenw238 <allenwu750@gmail.com>
2021-09-05 13:29:57 -0700

    My C++ Template

---
+++ codeforces/Template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const int MOD = 1000000007;
+
+// Solves one testcase:
+void solve() {
+
+}
+
+int main() {
+  // Comment out the below three lines if there is only one test case per test
+  // (uncomment if already commented out):
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+//  solve();
+  return 0;
+}
