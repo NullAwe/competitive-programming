bef2b2e30b95c6da244e89c9e0cc541bd382ea6b
Allen Wu <66089966+NullAwe@users.noreply.github.com>
2021-09-07 09:34:10 -0700

    CSES Problem Set, Sorting And Searching: Concert Tickets
    AC

---
+++ cses/sorting-and-searching/ConcertTickets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m; cin >> n >> m;
+  multiset<int> tck;
+  vector<int> ppl(m);
+  for (int i = 0; i < n; ++i) {
+    int a; cin >> a;
+	tck.insert(a);
+  }
+  for (int i = 0; i < m; ++i) {
+	int a; cin >> a;
+	auto it = tck.upper_bound(a);
+	// Output:
+	if (it == tck.begin()) cout << -1 << endl;
+	else {
+	  --it;
+	  cout << *it << endl;
+	  // Deletes element because each ticket can only be used once:
+	  tck.erase(it);
+	}
+  }
+  return 0;
+} 
+  
