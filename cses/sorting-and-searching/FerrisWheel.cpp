704226d2f5f0b5ff20a2671257d0c6563508018c
Allen Wu <66089966+NullAwe@users.noreply.github.com>
2021-09-07 08:40:42 -0700

    CSES Problem Set, Sorting and Searching: Ferris Wheel
    AC

---
+++ cses/sorting-and-searching/FerrisWheel.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, x; cin >> n >> x;
+  vector<int> arr(n); for (int i = 0; i < n; ++i) cin >> arr[i];
+  sort(arr.begin(), arr.end());
+  // Finds answer using greedy and two pointers:
+  int left = 0, right = n - 1, ans = 0;
+  while (left <= right) {
+    if (left == right || arr[left] + arr[right] > x) --right, ++ans;
+	  else ++left, --right, ++ans;
+  }
+  // Output:
+  cout << ans << endl;
+  return 0;
+}
