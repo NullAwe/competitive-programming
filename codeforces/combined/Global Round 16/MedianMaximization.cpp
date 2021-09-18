30c5de6733fc1d47af09b35c6a77448f43acbc4d
allenw238 <allenwu750@gmail.com>
2021-09-17 19:08:55 -0700

    Codeforces Global Round 16, Problem A. Median Maximization
    AC

---
+++ codeforces/combined/Global Round 16/MedianMaximization.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+
+void solve() {
+  int n, s; cin >> n >> s;
+  int lo = 0, hi = s + 1;
+  // Binary searches for the answer:
+  while (lo < hi - 1) {
+    int mid = (lo + hi) / 2;
+    ll sum = (ll) mid * (n / 2 + 1);
+    if (s - sum < 0) hi = mid;
+    else lo = mid;
+  }
+  // Output:
+  cout << lo << endl;
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
