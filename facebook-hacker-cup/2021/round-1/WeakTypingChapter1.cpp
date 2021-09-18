7b876fb353facf1c4617d2336908ddeb4f2fcffc
allenw238 <allenwu750@gmail.com>
2021-09-17 19:27:47 -0700

    FaceBook Hacker Cup - 2021: Round 1 Problem A1. Weak Typing - Chapter 1
    AC

---
+++ facebook-hacker-cup/2021/round-1/WeakTypingChapter1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t; cin >> t;
+  for (int i = 1; i <= t; ++i) {
+    int n; cin >> n;
+    string str; cin >> str;
+    int changes = 0;
+    char cur = ' ';
+    for (int j = 0; j < n; ++j) {
+      char ch = str[j];
+      if (ch == 'O' || ch == 'X') {
+        if (cur == ' ') cur = ch;
+        if (ch != cur) {
+          cur = ch;
+          ++changes;
+        }
+      }
+    }
+    cout << "Case #" << i << ": " << changes << endl;
+  }
+  return 0;
+}
