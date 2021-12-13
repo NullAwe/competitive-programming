daf6690794ed84c91b12d19d278ddecbe3446284
allenw238 <allenwu750@gmail.com>
2021-12-12 16:50:37 -0800

    Codeforces Round #758 (Div.1 + Div. 2) Problem A. Find Array
    AC

---
+++ codeforces/combined/Codeforces Round #758 (Div.1 + Div. 2)/FindArray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n; cin >> n;
+  // Output:
+  for (int i = 0; i < n; ++i) cout << i + 2 << ' ';
+  cout << endl;
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
