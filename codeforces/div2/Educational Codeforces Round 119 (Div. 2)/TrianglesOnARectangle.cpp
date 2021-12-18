fa4f1ea899a8be3a8ff4ea169638a5cd2f47fd28
allenw238 <allenwu750@gmail.com>
2021-12-18 11:40:58 -0800

    Educational Codeforces Round 119 (Rated for Div. 2) Problem B. Triangles On A Rectangle
    AC

---
+++ codeforces/div2/Educational Codeforces Round 119 (Div. 2)/TrianglesOnARectangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  ll w, h, ans = 0; cin >> w >> h;
+  for (int i = 0; i < 4; ++i) {
+    int k; cin >> k;
+    ll first = 0, last = 0;
+    for (int j = 0; j < k; ++j) {
+      int a; cin >> a;
+      if (first == 0) first = a;
+      last = a;
+    }
+    ans = max(ans, (i < 2 ? h : w) * (last - first));
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
