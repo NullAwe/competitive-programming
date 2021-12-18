f005d73f060439b2322fe2a63af8130a01ed64c8
allenw238 <allenwu750@gmail.com>
2021-12-18 11:37:38 -0800

    Educational Codeforces Round 119 (Rated for Div. 2) Problem A. Equal Or Not Equal
    AC

---
+++ codeforces/div2/Educational Codeforces Round 119 (Div. 2)/EqualOrNotEqual.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  string str; cin >> str;
+  int n = 0;
+  for (int i = 0; i < str.size(); ++i) if (str[i] == 'N') ++n;
+  cout << (n == 1 ? "NO" : "YES") << endl;
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
