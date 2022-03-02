794caa5e1e8e93c66ca14680cb9be9139f425744
allenw238 <allenwu750@gmail.com>
2022-03-01 18:57:40 -0800

    Experimental Educational Round: VolBIT Formulas Blitz Problem I. Parking Lot
    AC

---
+++ codeforces/random/ParkingLot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+ll p(int x, int y) {
+  ll ans = 1;
+  for (int i = 0; i < y; ++i) ans *= x;
+  return ans;
+}
+
+int main() {
+  int n; cin >> n;
+  cout << 6 * p(4, n - 2) + (n - 3) * 9 * p(4, n - 3) << '\n';
+}
