f98d489f76308fcd4ff449ef56251827649c6760
Allen Wu <66089966+NullAwe@users.noreply.github.com>
2022-03-10 09:03:17 -0800

    Educational Codeforces Round 124 (Div. 2) Problem A. Playoff
    AC

---
+++ codeforces/div2/Educational Codeforces Round 124 (Div. 2)/Playoff.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n; cin >> n;
+  cout << (1 << n) - 1 << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
