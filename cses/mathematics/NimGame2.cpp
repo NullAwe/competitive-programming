4e2999ccc24f36016abcb9cd5c3f6af75a247166
allenw238 <allenwu750@gmail.com>
2022-06-24 11:28:03 -0700

    CSES Problem Set, Mathematics: Nim Game 2
    AC

---
+++ cses/mathematics/NimGame2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+  int n, ans = 0; cin >> n;
+  for (int i = 0; i < n; ++i) { int x; cin >> x; ans ^= (x % 4); }
+  cout << (ans ? "first" : "second") << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
+
