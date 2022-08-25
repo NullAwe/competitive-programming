cce84f5c384f17e74d6b5b2472adc91b92be17d1
allenw238 <allenwu750@gmail.com>
2022-08-24 21:26:13 -0700

    Codeforces Round #814 (Div. 1) Problem A1. Burenka and Traditions (easy version)
    AC

---
+++ codeforces/div1/Codeforces Round #814 (Div. 1)/A1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+  int n; cin >> n;
+  vector<int> arr(n); for (int i = 0; i < n; ++i) cin >> arr[i];
+  int ans = 0, last = -1;
+  for (int i = 0; i < n; ++i) {
+    int _xor = 0;
+    for (int j = i - 1; j > last; --j) {
+      if (_xor == arr[i]) {
+        last = i;
+        break;
+      }
+      _xor ^= arr[j];
+    }
+    if (last == i) continue;
+    if (_xor == arr[i]) {
+      last = i;
+      continue;
+    }
+    ++ans;
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
