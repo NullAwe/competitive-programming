2faf78c3333d7cd1f59c60bfe66571492de0b9d4
allenw238 <allenwu750@gmail.com>
2022-08-28 09:13:39 -0700

    Baltic Olympiad In Informatics 2022 Day 2 Problem 1. Flight To The Ford
    90/100

--- boi/2022/day2/FlightToTheFord.cpp
+++ boi/2022/day2/FlightToTheFord.cpp
@@ -5,54 +5,46 @@ using namespace std;
 inline void s(int& x, int b, int v) { x -= (x & (1 << b)), x |= v << b; }
 
 void encode(int n, int x) {
-  set<int> todo; for (int i = 0; i < 30; ++i) todo.insert(i);
+  int bits = 1;
+  while ((1 << bits) <= n) ++bits;
+  set<int> todo; for (int i = 0; i < bits; ++i) todo.insert(i);
   vector<pair<int, int>> same, diff;
-  int a = 0, b = 0; b |= 1;
+  int a = 0, b = 1, l = -1, last = -1;
   while (todo.size() > 1) {
     int b1 = *todo.begin(), b2 = *++todo.begin();
     vector<int> res(4);
-    res[0] = send((x >> b1) & 1);
+    if (b1 == last) res[0] = l;
+    else res[0] = send((x >> b1) & 1);
     res[1] = send((x >> b2) & 1);
     res[2] = send((x >> b2) & 1);
-    res[3] = send((x >> b1) & 1);
     if (res[1] == res[2]) {
       a |= res[1] << b2, b |= res[1] << b2;
       todo.erase(b2);
-    } else if (res[0] == res[3]) {
+      l = res[2], last = b2;
+      continue;
+    }
+    res[3] = send((x >> b1) & 1);
+    l = res[3], last = b1;
+    if (res[0] == res[3]) {
       s(a, b1, res[0]), s(b, b1, res[0]), s(b, b2, 1);
-      int cur = b1;
-      while (same.size() || diff.size()) {
-        pair<int, int> l;
-        if (same.size()) {
-          for (int i = 0; i < same.size(); ++i) {
-            if (same[i].second == cur) {
-              swap(same[i], same[same.size() - 1]);
-              break;
-            }
-          }
-          l = same.back();
-          if (l.second == cur) {
-            s(a, l.first, (a >> cur) & 1);
-            s(b, l.first, (b >> cur) & 1);
-            cur = l.first;
-            same.resize(same.size() - 1);
-            continue;
-          }
-        }
-        if (!diff.size()) break;
-        for (int i = 0; i < diff.size(); ++i) {
-          if (diff[i].second == cur) {
-            swap(diff[i], diff[diff.size() - 1]);
-            break;
-          }
-        }
-        l = diff.back();
-        if (l.second != cur) break;
-        s(a, l.first, 1 - ((a >> cur) & 1));
-        s(b, l.first, 1 - ((b >> cur) & 1));
-        cur = l.first;
-        diff.resize(diff.size() - 1);
+      int taken = 0;
+      for (int i = 0; i < same.size() - taken; ++i)
+        if (same[i].first == b1)
+          swap(same[i], same[same.size() - 1 - taken++]), --i;
+      for (int i = 0; i < taken; ++i) {
+        s(a, same[same.size() - 1 - i].second, (a >> b1) & 1);
+        s(b, same[same.size() - 1 - i].second, (b >> b1) & 1);
       }
+      same.resize(same.size() - taken);
+      taken = 0;
+      for (int i = 0; i < diff.size() - taken; ++i)
+        if (diff[i].first == b1)
+          swap(diff[i], diff[diff.size() - 1 - taken++]), --i;
+      for (int i = 0; i < taken; ++i) {
+        s(a, diff[diff.size() - 1 - i].second, 1 - ((a >> b1) & 1));
+        s(b, diff[diff.size() - 1 - i].second, 1 - ((b >> b1) & 1));
+      }
+      diff.resize(diff.size() - taken);
       todo.erase(b1);
     } else {
       int pa = (res[0] == res[1]), pb = 3 - pa;
@@ -62,57 +54,52 @@ void encode(int n, int x) {
       else a |= (pa & 1) << b2;
       if (b & (1 << b1)) b |= (pb & 1) << b2;
       else b |= (pa & 1) << b2;
-      todo.erase(b1);
+      todo.erase(b2);
     }
   }
 }
 
 pair<int, int> decode(int n) {
-  set<int> todo; for (int i = 0; i < 30; ++i) todo.insert(i);
+  int bits = 1;
+  while ((1 << bits) <= n) ++bits;
+  set<int> todo; for (int i = 0; i < bits; ++i) todo.insert(i);
   vector<pair<int, int>> same, diff;
-  int a = 0, b = 0; b |= 1;
+  int a = 0, b = 1, l = -1, last = -1;
   while (todo.size() > 1) {
     int b1 = *todo.begin(), b2 = *++todo.begin();
     vector<int> res(4);
-    for (int i = 0; i < 4; ++i) res[i] = receive();
+    if (b1 == last) res[0] = l;
+    else res[0] = receive();
+    res[1] = receive();
+    res[2] = receive();
     if (res[1] == res[2]) {
       a |= res[1] << b2, b |= res[1] << b2;
       todo.erase(b2);
-    } else if (res[0] == res[3]) {
+      l = res[2], last = b2;
+      continue;
+    }
+    res[3] = receive();
+    l = res[3], last = b1;
+    if (res[0] == res[3]) {
       s(a, b1, res[0]), s(b, b1, res[0]), s(b, b2, 1);
-      int cur = b1;
-      while (same.size() || diff.size()) {
-        pair<int, int> l;
-        if (same.size()) {
-          for (int i = 0; i < same.size(); ++i) {
-            if (same[i].second == cur) {
-              swap(same[i], same[same.size() - 1]);
-              break;
-            }
-          }
-          l = same.back();
-          if (l.second == cur) {
-            s(a, l.first, (a >> cur) & 1);
-            s(b, l.first, (b >> cur) & 1);
-            cur = l.first;
-            same.resize(same.size() - 1);
-            continue;
-          }
-        }
-        if (!diff.size()) break;
-        for (int i = 0; i < diff.size(); ++i) {
-          if (diff[i].second == cur) {
-            swap(diff[i], diff[diff.size() - 1]);
-            break;
-          }
-        }
-        l = diff.back();
-        if (l.second != cur) break;
-        s(a, l.first, 1 - ((a >> cur) & 1));
-        s(b, l.first, 1 - ((b >> cur) & 1));
-        cur = l.first;
-        diff.resize(diff.size() - 1);
+      int taken = 0;
+      for (int i = 0; i < same.size() - taken; ++i)
+        if (same[i].first == b1)
+          swap(same[i], same[same.size() - 1 - taken++]), --i;
+      for (int i = 0; i < taken; ++i) {
+        s(a, same[same.size() - 1 - i].second, (a >> b1) & 1);
+        s(b, same[same.size() - 1 - i].second, (b >> b1) & 1);
       }
+      same.resize(same.size() - taken);
+      taken = 0;
+      for (int i = 0; i < diff.size() - taken; ++i)
+        if (diff[i].first == b1)
+          swap(diff[i], diff[diff.size() - 1 - taken++]), --i;
+      for (int i = 0; i < taken; ++i) {
+        s(a, diff[diff.size() - 1 - i].second, 1 - ((a >> b1) & 1));
+        s(b, diff[diff.size() - 1 - i].second, 1 - ((b >> b1) & 1));
+      }
+      diff.resize(diff.size() - taken);
       todo.erase(b1);
     } else {
       int pa = (res[0] == res[1]), pb = 3 - pa;
@@ -122,7 +109,7 @@ pair<int, int> decode(int n) {
       else a |= (pa & 1) << b2;
       if (b & (1 << b1)) b |= (pb & 1) << b2;
       else b |= (pa & 1) << b2;
-      todo.erase(b1);
+      todo.erase(b2);
     }
   }
   return {max(min(a, n), 1), max(min(b, n), 1)};
