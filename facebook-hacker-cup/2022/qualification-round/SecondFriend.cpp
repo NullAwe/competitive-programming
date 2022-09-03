f5cc1b89b145f5c815762907e0acdb555befb8bf
allenw238 <allenwu750@gmail.com>
2022-09-03 10:51:51 -0700

    Facebook Hacker Cup - 2022: Qualification Round Problem B1. Second Friend
    AC

---
+++ facebook-hacker-cup/2022/qualification-round/SecondFriend.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int t; cin >> t;
+  for (int i = 1; i <= t; ++i) {
+    int n, m; cin >> n >> m;
+    vector<string> arr(n); for (int j = 0; j < n; ++j) cin >> arr[j];
+    if (n > 1 && m > 1) {
+      cout << "Case #" << i << ": Possible\n";
+      for (int j = 0; j < n; ++j) cout << string(m, '^') << '\n';
+      continue;
+    }
+    int ts = 0;
+    for (int j = 0; j < n; ++j) for (int k = 0; k < m; ++k) ts += arr[j][k] == '^';
+    if (ts) cout << "Case #" << i << ": Impossible\n";
+    else {
+      cout << "Case #" << i << ": Possible\n";
+      for (string str : arr) cout << str << '\n';
+    }
+  }
+  return 0;
+}
