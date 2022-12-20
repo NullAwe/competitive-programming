cdd7c3d0b7ce2f39acb27feac526a3123c33f2c2
allenw238 <allenwu750@gmail.com>
2022-12-20 15:00:00 -0800

    Codeforces Global Round 24 Problem A. Doremy's Paint
    AC

---
+++ codeforces/combined/Global Round 24/DoremysPaint.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n; cin >> n;
+  for (int i = 0; i < n; ++i) {int x; cin >> x;}
+  cout << 1 << ' ' << n << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
