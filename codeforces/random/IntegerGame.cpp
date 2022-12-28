9e39dcd12af8573aded9da6937dbf90b3729d11d
allenw238 <allenwu750@gmail.com>
2022-12-28 11:41:24 -0800

    Codeforces Global Round 9 Problem F. Integer Game
    AC

---
+++ codeforces/random/IntegerGame.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+
+ll nums[3];
+
+void query(ll x) {
+  cout << x << endl;
+  int ans; cin >> ans;
+  if (ans <= 0) exit(0);
+  nums[ans - 1] += x;
+}
+
+int main() {
+  cin >> nums[0] >> nums[1] >> nums[2];
+  cout << "First" << endl;
+  query(1e9);
+  vector<ll> tmp(3);
+  for (int i = 0; i < 3; ++i) tmp[i] = nums[i];
+  sort(tmp.begin(), tmp.end());
+  query(tmp[2] + (tmp[2] - tmp[0]) - tmp[1]);
+  for (int i = 0; i < 3; ++i) tmp[i] = nums[i];
+  sort(tmp.begin(), tmp.end());
+  query(tmp[1] - tmp[0]);
+  return 0;
+}
