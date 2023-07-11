a30fd35c2d34a96a91b750f1d88eff661002f2df
NullAwe <allenwu750@gmail.com>
2023-07-11 13:56:04 -0700

    Codeforces Round 884 (Div. 1 + Div. 2) Problem A. Subtraction Game
    AC

---
+++ codeforces/combined/Codeforces Round #884 (Div. 1 + Div. 2)/SubtractionGame.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b; cin >> a >> b;
+  cout << a + b << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  for (int i = 0; i < t; ++i) solve();
+  return 0;
+}
